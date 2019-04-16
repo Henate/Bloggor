06f4ea618a5543bf4109aefee5ccb9fd27977da7
Henate <Henate@126.com>
2019-04-16 23:35:06 +0800

    Add Router管理

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
+	"Bloggor/routers"
 	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 
 	"Bloggor/pkg/setting"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
