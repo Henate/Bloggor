759693291da76ad97c0b54e9de547052ab14edf2
Henate <Henate@126.com>
2019-04-17 23:39:35 +0800

    Add models callbacks

--- models/tag.go
+++ models/tag.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type Tag struct {
 	Model
 
@@ -40,4 +45,16 @@ func AddTag(name string, state int, createdBy string) bool{
 	})
 
 	return true
+}
+
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
 }
